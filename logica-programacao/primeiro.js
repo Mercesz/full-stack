0016c3acb88796a034ff7f8870b9248507ef1d6c
Mercez <ezequielmerces36@gmail.com>
2025-11-10 15:10:56 -0300

    feat: Adicionando lógica na soma dos números

---
+++ logica-programacao/primeiro.js
@@ -0,0 +1,14 @@
+//Perguntando o nome para o usuário
+const userName = prompt("Qual é o seu nome? ")
+
+//Perguntando qual é a sua idade
+const userAge = prompt("Qual é a sua idade? ")
+
+
+// Imprimindo na tela o resultado
+document.write(`Seu nome é: ${"<strong>"} ${userName} ${"</strong>"}`)
+
+//Imprimindo na tela a idade
+document.write(`${"<br>"}Sua idade é : ${"<strong>"} ${userAge} ${"</strong>"} Anos `)
+
+
---
+++ logica-programacao/segundo.js
@@ -0,0 +1,4 @@
+let num1 = parseInt(prompt("Digite um número: "))
+let num2 = parseInt(prompt("Digite outro: "))
+
+console.log(num1 + num2)
