52b1f0912b4cc8234ae753232f760a91ba515684
Mercez <ezequielmerces36@gmail.com>
2025-11-18 16:19:47 -0300

    Adicionando listagem na função do JS

--- smart-contatos/JS/script.js
+++ smart-contatos/JS/script.js
@@ -1,4 +1,8 @@
+// Chamando a tag FORM
 const form = document.querySelector("form");
+
+//Chamando a classe lista do HTML
+const lista = document.querySelector(".lista");
 
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
@@ -8,16 +12,26 @@ form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     //Validação do formulário
-    /*
-        Se input for vazio, adicionar uma mensagem
-    */
-
     if (inputNome.value == "" || inputEmail.value == "" || inputTel.value == "") {
-
         alert("Campos em brancos, por favor preencha os dados.");
-        return false;  
+        return false;
     }
 
-    console.log(inputNome.value, inputEmail.value, inputTel.value);
+    //Criando LI
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        <span class="contato-nome">Nome: ${inputNome.value}</span>
+        <span class="contato-email">Email: ${inputEmail.value}</span>
+        <span class="contato-telefone">Telefone: ${inputTel.value}</span>
+    `;
+
+    // enviando os dados da lista appendChild
+    lista.appendChild(li);
+
+
+    //Resetando os valores dos inputs
+    form.reset();
+
 });
 
