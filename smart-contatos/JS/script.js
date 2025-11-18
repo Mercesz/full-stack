b13a2614e243f5b128766dcba8421e772f0a5678
Mercez <ezequielmerces36@gmail.com>
2025-11-18 15:05:20 -0300

    Adicionando validação do formulário, removendo o onclick do botao

--- smart-contatos/JS/script.js
+++ smart-contatos/JS/script.js
@@ -1,10 +1,23 @@
+const form = document.querySelector("form");
+
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTel = document.getElementById("telefone");
 
-function btnCadastrar(event) {
-
-    console.log(inputNome.value, inputEmail.value, inputTel.value);
+form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-}
+    //Validação do formulário
+    /*
+        Se input for vazio, adicionar uma mensagem
+    */
+
+    if (inputNome.value == "" || inputEmail.value == "" || inputTel.value == "") {
+
+        alert("Campos em brancos, por favor preencha os dados.");
+        return false;  
+    }
+
+    console.log(inputNome.value, inputEmail.value, inputTel.value);
+});
+
