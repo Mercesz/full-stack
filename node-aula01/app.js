486923065c14ca00ffa176b1ea9b5c8de6324062
Mercez <ezequielmerces36@gmail.com>
2025-11-24 16:26:06 -0300

    Adicionando novas rotas no arquivo APP.JS e incluindo nova página Home html e estilizando-a

--- node-aula01/app.js
+++ node-aula01/app.js
@@ -2,11 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//Varioável com os dados produtos
+const produtos = [
+    { id: 1, nome: 'Produto A', preco: 100 },
+    { id: 2, nome: 'Produto B', preco: 150 },
+    { id: 3, nome: 'Produto C', preco: 200 }
+];
+
 //Criando uma função
 // Rota principal
 app.get('/', (req, res) => {
     res.send('Bem vindo ao servidor Node.JS');
 });
+
+// Rota produtos
+app.get('/produtos', (req, res) => {
+    res.send(produtos);
+});
+
+//Rota Home
+app.get('/home', (req, res) => {
+    // console.log("Este é o dirname", __dirname)
+    res.sendFile(__dirname + '/public/home.html');
+});
+
 
 // Executando o servidor
 app.listen(port, () => {
