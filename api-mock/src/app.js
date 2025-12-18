e42070d7a4765e05a23d1477bffff38d891bb205
Mercez <ezequielmerces36@gmail.com>
2025-12-18 14:00:13 -0300

    adicionando rotas de adicionar e deletar

--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -3,9 +3,29 @@ import conexao from '../infra/conexao.js'
 
 const app = express();
 
-
+// Rota principal
 app.get('/', (req, res) => {
     res.send("Ola Node.JS");
+});
+
+// Rota de cadastro
+app.post('/selecoes', (req, res) => {
+    const selecao = req.body;
+    const sql = 'INSERT INTO selecoes SET?;';
+
+    conexao.query(sql, selecao => {
+        res.json({ mensagem: "Cadastrado com sucesso!" });
+    });
+});
+
+// Rota de Delete
+app.delete('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = 'delete from selecoes where id=?';
+
+    conexao.query(sql, id => {
+        res.json({ mensagem: "Deletando com sucesso!" })
+    })
 });
 
 
