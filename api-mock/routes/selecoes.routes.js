8bf39bb0ee1a3c677bf1b85dd74170b7e7eaa406
Mercez <ezequielmerces36@gmail.com>
2025-12-18 16:19:10 -0300

    feat: refatorando as rotas de alterar e deletar

--- api-mock/routes/selecoes.routes.js
+++ api-mock/routes/selecoes.routes.js
@@ -12,6 +12,48 @@ router.get('/selecoes', (req, res) => {
     });
 });
 
+// Buscando por ID
+router.get('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "select * from selecoes where id=?;";
+
+    conexao.query(sql, id, (erro, result) => {
+        res.json(result[0]);
+    });
+});
+
+// Criando POST para cadastrar 
+router.post('/selecoes', (req, res) => {
+    const selecao = req.body;
+    const sql = "INSERT INTO selecoes SET?;";
+
+    conexao.query(sql, selecao, () => {
+        res.json({ mensagem: "Cadastrado com sucesso!" });
+    })
+});
+
+// Deletando registro
+router.delete('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "delete from selecoes where id=?";
+
+    conexao.query(sql, id, () => {
+        res.json({ menssagem: "Deletando com sucesso" });
+    });
+});
+
+// Atualizando registro teste
+router.put('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const selecao = req.body;
+    const sql = "update selecoes set ? where id=?";
+
+    conexao.query(sql, [selecao, id], () => {
+        res.json({ mensagem: "Atualizando com sucesso!" });
+    });
+});
+
+
 export default router;
 
 
--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -12,8 +12,14 @@ app.get("/", (req, res) => {
     res.send("Olá Copa do Mundo!");
 });
 
+
 // Usando a rotas de seleções
 app.use(selecoesRoutes);
 
+
+// Rota 404
+app.use((req, res) => {
+    res.status(404).json({ mensagem: "Rota não encontrada" });
+});
 
 export default app;
